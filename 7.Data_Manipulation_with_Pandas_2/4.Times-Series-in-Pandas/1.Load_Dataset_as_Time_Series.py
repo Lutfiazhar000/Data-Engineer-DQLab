87b8204891bda6a2cefccab55f29cbc7cefd2474
lutfiazhar000 <asharilutfi71@gmail.com>
2022-06-25 12:11:54 +0700

    my recap study

---
+++ 7.Data_Manipulation_with_Pandas_2/4.Times-Series-in-Pandas/1.Load_Dataset_as_Time_Series.py
@@ -0,0 +1,14 @@
+# Terkadang Pandas salah mengenali object datetime menjadi object string dan pandas menjadi tidak bisa memanfaatkan full benefit dari time index.
+
+# Basic format datetime menurut ISO 8601: YYYY-mm-dd HH:MM:SS.
+
+# Terdapat beberapa cara untuk mengubah kolom waktu menjadi format yang benar as datetime object di Pandas. 
+
+import pandas as pd
+# Load dataset https://storage.googleapis.com/dqlab-dataset/LO4/global_air_quality_4000rows.csv
+gaq = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/LO4/global_air_quality_4000rows.csv', parse_dates=True, index_col='timestamp')
+# Cetak 5 data teratas
+print(gaq.head())
+# Cetak info dari dataframe gaq
+print('info')
+print(gaq.info())
